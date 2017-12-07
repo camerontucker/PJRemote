34ce0e4d6733ea1a7cfb22ce4ceb848134468383
Cam Tucker <ctucker@regent-college.edu>
2017-12-07 13:12:43 -0800

    Initial Commit

---
+++ PJRemote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PJRemote
+//
+//  Created by Cam Tucker on 2017-12-07.
+//  Copyright © 2017 Regent College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
